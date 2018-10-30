4d008f719d73d0ce7510aea199744c6f535abcc9
Vonds <vonds13@gmail.com>
2018-10-30 18:59:04 -0400

    14th codewar

---
+++ fourteen.js
@@ -0,0 +1,13 @@
+function digitize(n) {
+// Created an empty string to put numbers in
+  let array = [],
+      // converted number variable into a string
+      sNumber = n.toString();
+  // Using for loop to put numbers into empty array
+  for (let i = 0, len = sNumber.length; i < len; i += 1) {
+  // Pushing numbers into array
+    array.push(+sNumber.charAt(i));
+  }
+  // Reversing array items
+  return array.reverse()
+}
