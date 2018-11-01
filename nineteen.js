e211a4a19c1105ab1338f80af23e816b3581f009
Vonds <vonds13@gmail.com>
2018-11-01 10:32:03 -0400

    19th codewar

---
+++ nineteen.js
@@ -0,0 +1,18 @@
+/*
+You will be given an array(a) and a value(x). All
+you need too do is check whether the provided array
+contains the value.
+
+Array can contain numbers or strings. X can be either.
+Return true if the array contains the value, false if not
+*/
+
+function check(a, x){
+  for(let i = 0; i < a.length; i++){
+    if(a[i] === x){return true}
+  }
+  return false
+}
+
+// An alternative way to arrive at the same answer
+const check = (a,x) => a.include(x);
