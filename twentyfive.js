e1d7dcc9e510b27bddc40b5ffa604cd9928b96ee
Vonds <vonds13@gmail.com>
2018-11-02 10:25:59 -0400

    25th codewar

---
+++ twentyfive.js
@@ -0,0 +1,8 @@
+/* In this simple assignment you are given a number and have to make it negative.
+But maybe the number is already negative? */
+
+const makeNegative = num => (Math.sign(num) === 1? -num: num)
+
+// Alternative
+
+makeNegative = num => -Math.abs(num)
