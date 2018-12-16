f4c5835b0fa3cb40d127646fb6e1f367cad26681
Vonds <vonds13@gmail.com>
2018-12-16 09:40:39 -0500

    42th codewar

---
+++ fortytwo.js
@@ -0,0 +1,22 @@
+/*
+    Take an array and remove every second element out of that array. Always keep the first element and start removing with the next element.
+    myArr = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...];
+    None of the arrays will be empty, so you don't have to worry about that!
+*/
+
+const removeEveryOther = arr => {
+    // create an empty array to push all the answers into
+    let answer = []
+    // loop through array using map method w/ variables current and index
+    arr.map((current, i) => {
+        // if variable i modulus 2 is zero then push the current variable into answer
+        if (i % 2 === 0) {
+            answer.push(current)
+        }
+    })
+    // return the new array with the even i remo
+    return answer
+}
+
+
+
